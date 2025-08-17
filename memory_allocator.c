dee9cfbe5af14ea4474e08d0703d6f4d34bad1c7
andrewseo2001 <andrewseo2001@gmail.com>
2025-08-16 21:01:22 -0400

    fixed bug in calloc and realloc

--- memory_allocator.c
+++ memory_allocator.c
@@ -1,19 +1,25 @@
-  typedef char ALIGN[16];
+  #include <unistd.h>
+  #include <stdio.h>
+  #include <pthread.h>
+  #include <string.h>
+  #include <stdalign.h>
+  #include <stddef.h>   // defines max_align_t
+
 
   union header {
 
     struct {
       size_t size;
       unsigned is_free;
-      struct header_t *next;
+      union header *next;
     } s;
    
-    ALIGN stub;
-  }
+    max_align_t _align;
+  };
 
   typedef union header header_t;
   header_t *head, *tail;
-  pthread_mutex_t global_malloc_lock;
+  pthread_mutex_t global_malloc_lock = PTHREAD_MUTEX_INITIALIZER;
 
   header_t *get_free_block(size_t size){
     header_t *curr = head;
@@ -23,13 +29,12 @@
       curr = curr->s.next;
     }
     return NULL;
-  
+  }
 
   void *malloc(size_t size) {
     size_t total_size;
     void *block;
     header_t *header;
-
     if(!size)
       return NULL;
 
@@ -74,19 +79,77 @@
       return;
 
     pthread_mutex_lock(&global_malloc_lock);
+    //check the address of the current block
     header = (header_t*)block - 1;
     programbreak = sbrk(0);
+    //if the end of the block is at the program break 
     if((char*)block + header->s.size == programbreak){
       if (head == tail){
         head = tail = NULL;
       }else{
         tmp = head;
+        //keep iterating until you reach the last node before tail
+        //free the node and set curr node to tal
         while(tmp) {
-
+          if(tmp->s.next == tail){
+              tmp->s.next = NULL;
+              tail = tmp;
+            }
+            tmp = tmp->s.next;
         }
       }
+      sbrk(0 - sizeof(header_t) - header->s.size);
+      pthread_mutex_unlock(&global_malloc_lock);
+      return;
     }
-    
 
+    header->s.is_free = 1;
+    pthread_mutex_unlock(&global_malloc_lock);
   }
-  
+
+  void *calloc(size_t num, size_t nsize){
+    size_t size;
+    void *block;
+    if (!num || !nsize)
+      return NULL;
+    size = num * nsize;
+    //check null overflow
+    if(nsize != size/num)
+      return NULL;
+
+    block = malloc(size);
+    if(!block)
+      return NULL;
+    
+    memset(block, 0, size);
+    return block;    
+  }
+
+  void *realloc(void *block, size_t size){
+    header_t *header;
+    void *ret;
+
+    if (!block) {
+      return size ? malloc(size): NULL;
+    }
+
+    if (size == 0){
+      free(block);
+      return NULL;
+    }
+
+    header = (header_t*)block - 1;
+    if (header->s.size >= size)
+      return block;
+    ret = malloc(size);
+    if (ret) {
+      memcpy(ret, block, header->s.size);
+      free(block);
+    }
+      return ret;
+  }
+
+
+ 
+
+
